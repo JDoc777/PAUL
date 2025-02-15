f69d832e53b46540f2156dcbf6f967a068c279cc
Keven20041 <116309709+Keven20041@users.noreply.github.com>
2025-02-15 18:30:15 -0500

    Used pydoc on the file
    Going to be pydocing every file to make sure I understand how it all works and also for those who read the files in the future so we don't have to repeat ourselves

--- OOP/common.py
+++ OOP/common.py
@@ -1,3 +1,36 @@
+"""
+Wheel Position Modules
+
+This module will provide the details of the wheel positions for the robot control system of PAUL.
+It includes global variables for the wheel positions and has a function to retrieve those wheel position as a dictionary for OOP purposes.
+
+Global Varibles included:
+    wheel_pos (list): A list of four integers representing the positions of the wheels.
+    The order of the wheels are [Front Left, Back Left, Front Right, Back Right].
+
+    Functions:
+    get_wheel_positions(): returns the position of the wheels and thier values from the list
+
+    Usage:
+    This module can be imported as a module for the robot control system of PAUL.
+
+    Example for those who aren't experts at python:
+    from wheel_position import wheel_pos, get_wheel_dic
+
+    Dependecies ( Also known as Libraries):
+    json
+    time
+    queue
+    serial
+    threading
+    sys
+    Encoder
+    array
+    Velocity
+    PrintQueue
+    rwSerial
+"""
+
 import json
 import time
 import queue
@@ -14,6 +47,13 @@ import rwSerial
 
 
 wheel_pos = [0, 0, 0, 0]
+
+# get_wheel_dict allows you to
+# return a list of the wheel positions.
+# FL (Front Left) index 0
+# BL (Back Left) index 1
+# FR (Front Right) index 2
+# BR (Back Right) index 3
 
 def get_wheel_dict():
     return {
